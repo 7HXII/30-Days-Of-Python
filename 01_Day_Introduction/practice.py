c3315edf1f9b77cf778fe9c271b33fea6c88262f
7HXII <232992979+7HXII@users.noreply.github.com>
2026-02-12 22:29:11 +0530

    practice.py

--- 01_Day_Introduction/practice.py
+++ 01_Day_Introduction/practice.py
@@ -6,8 +6,28 @@ print(2*4)#multiply
 print(2**4) #exponent
 print(4/2)#division
 print(6//4) #floor division/round off
+print(4<6)#less than
+print(4>2)#greater than
+print(8==8)#equal
+print(5!=3)#not equal
+print(6<=4)#less than or equal 
+print(5>=9)#greater than or equal
 
-#checking data types &
+
+#assignment operators
+x = 6# use variable
+x /= 2 #x = x / 2
+print(x)
+
+y = 6 # use variable
+y %= 5#y = y % 5
+print(y)
+
+z = 5 #use variable
+z **= 3#z = z ** 3
+print(z)
+
+#checking data types 
 int = 10
 print(type(int))
 
@@ -21,7 +41,6 @@ string = "albert"
 print(type(string))
 
 list = [1, True, 3.4, "john"]
-list.append ''
 print(type(list))
 
 tuple = (1, 2, 3, 5)
@@ -33,3 +52,24 @@ print(type({1, 2, 3, 4, 5})) #dual ways to check dtype
 dict ={'salary':'20000'}
 print(type(dict))
 print(len(dict)) #check length
+
+#list operations
+list = [1, 2, 3, 4, 5]
+#indexing
+print(list[0])#first element
+print(list[-1])#last element
+#slicing[start:stop:step]
+print(list[::2])#[1, 3, 5]
+print(list[::-1])#reverse list
+print(list[1:2:2])
+list = [1, 2, 3, 4, 5]
+list.append(6)
+print(list)
+list.insert(2,10)#(index,value)
+print(list)
+list.remove(4)
+print(list)#removes the value
+list.sort(reverse = True)
+print(list)#reverse list
+list.clear()#clear list
+print(list)
